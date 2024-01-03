9ac7d622d78eccb966b7cf682280f1bd5c215721
Khoirunnajwa <148507944+Khoirunnajwa@users.noreply.github.com>
2024-01-03 08:09:12 +0700

    APLIKASI PENGARSIPAN SURAT

---
+++ profil_user.php
@@ -0,0 +1,92 @@
+
+<div class="">
+		<div class="page-title">
+			<div class="title_left">
+				<h3>Profil</h3>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="x_panel">
+					<div class="x_title">
+						<h2>Profil user</h2>&nbsp;
+						<?php 
+							$query	= "SELECT * FROM user";
+							$sql	= mysqli_query($connect, $query);
+							$data   = mysqli_fetch_array($sql);
+							if ($_SESSION['level']== 'admin') {
+							?>
+								<a href="index.php?page=edit_users&id=<?php echo $data['id'] ?>" class="btn btn-warning btn-sm"><i class="fa fa-pencil-square-o"></i> Edit Profil</a>'
+						<?php		
+							}
+						?>
+						<ul class="nav navbar-right panel_toolbox">
+							<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+							</li>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div class="x_content">
+						<div class="row" align="center">
+							<div class="col-md-12 col-sm-10 col-xs-12">
+								<div class="pricing">
+									<div class="title">
+										<center>
+											<img src="upload/user/<?php echo $_SESSION['foto'] ?>" alt="" class="img-circle img-responsive" style=" width: 100px; height: 80px;">
+										</center>
+									</div>	
+									<div class="x_content">
+										<div class="">
+											<div class="pricing_features">
+												<ul class="list-unstyled">
+													<center>
+														<div class="">
+															<strong>Fullname</strong><br>
+															<p><?php echo $data['fullname']; ?></p>
+															<hr>
+														</div>
+														<div class="">
+															<strong>Username</strong>
+															<p><?php echo $data['username']; ?></p>
+															<hr>
+														</div>	
+														<div class="">
+															<strong>Level</strong>
+															<p><?php echo $data['level'] ?></p>
+															<hr>
+														</div>
+														<div class="">
+															<strong>Jenis Kelamin</strong>
+															<p><?php echo $data['jenis_kelamin'] ?></p>
+															<hr>
+                                                        </div>
+														<div class="">
+															<strong>No Telepon</strong>
+															<p><?php echo $data['no_telepon'] ?></p>
+															<hr>
+                                                        </div>
+														<div class="">
+															<strong>Umur</strong>
+															<p><?php echo $data['umur'] ?></p>
+															<hr>
+                                                        </div>
+														<div class="">
+															<strong>Alamat</strong>
+															<p><?php echo $data['alamat'] ?></p>
+															<hr>
+                                                        </div>
+													</center>	
+												</ul>
+											</div>		
+										</div>
+									</div>
+								</div>
+							</div>						
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>		
+	</div>
